caa833c2600b6b43c1ea1731f12b205eb4e9b8fa
Fistello <44226196+Fistello@users.noreply.github.com>
2018-10-17 15:00:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Сегодня в городе " + location.get('city') + ": " + condition.get('text') + \
+             ", температура " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
